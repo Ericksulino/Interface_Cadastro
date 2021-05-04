79158b54460263bb6e439439b97fb57c47991835
Ericksulino Moura <62266038+Ericksulino@users.noreply.github.com>
2021-05-04 15:55:04 -0300

    criando arquivo para gerenciar as multiplas telas

---
+++ main_multitela.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMessageBox,QMainWindow,QApplication,QFileDialog
+from PyQt5.QtCore import QCoreApplication
+
+
+from pessoa import Pessoa
+from cadastro import Cadastro
+from tela_inicio import Tela_inicio
+from tela_cadastro import Tela_cadastro
+from tela_busca import Tela_busca
