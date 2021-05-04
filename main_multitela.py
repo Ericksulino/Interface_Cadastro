82df195942091e4f0fc7897b0b29f8b90240e5d1
Ericksulino Moura <62266038+Ericksulino@users.noreply.github.com>
2021-05-04 16:39:40 -0300

    Classe Ui_Main da main_multitela criada

--- main_multitela.py
+++ main_multitela.py
@@ -11,3 +11,32 @@ from cadastro import Cadastro
 from tela_inicio import Tela_inicio
 from tela_cadastro import Tela_cadastro
 from tela_busca import Tela_busca
+
+class Ui_Main(QtWidgets.QWidget):
+    def setupUi(self, Main):
+        Main.setObjectName('Main')
+        Main.resize(640,480)
+
+        self.QtStack = QtWidgets.QStackedLayout()
+
+        self.stack0 = QtWidgets.QMainWindow()
+        self.stack1 = QtWidgets.QMainWindow()
+        self.stack2 = QtWidgets.QMainWindow()
+
+        self.tela_inicio = Tela_inicio()
+        self.tela_inicio.setupUi(self.stack0)
+
+        self.tela_cadastro = Tela_busca()
+        self.tela_cadastro.setupUi(self.stack1)
+
+        self.tela_busca = Tela_busca()
+        self.tela_busca.setupUi(self.stack2)
+
+        self.QtStack.addWidget(self.stack0)
+        self.QtStack.addWidget(self.stack1)
+        self.QtStack.addWidget(self.stack2)
+        
+
+        
+
+
